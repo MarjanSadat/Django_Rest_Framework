6776501a73d22db158b5a10a85c5f456ba320247
MarjanSadat <marjanmousavi@ymail.com>
2022-02-06 12:14:21 +0330

    added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from codecs import lookup_error
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
+from blog.models import Article
+from django.contrib.auth.models import User
+from .serializers import ArticleSerializer, UserSerializer
+from .permissions import IsSuperUser, IsAuthorOrReadOnly, IsStaffOrReadOnly, IsSuperUserOrStaffReadOnly
 
-# Create your views here.
+# class ArticleList(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+class ArticleList(ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    permission_classes = ( IsAuthorOrReadOnly,)
+    # lookup_field = 'slug'
+
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsSuperUserOrStaffReadOnly,)
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsSuperUserOrStaffReadOnly,)
